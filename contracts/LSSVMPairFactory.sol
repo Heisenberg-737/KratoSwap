81bd53364f06c69097dfbf51b5e44ac25f44c8c7
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-03-23 23:07:24 +0530

    LSSVMPair Files

---
+++ contracts/LSSVMPairFactory.sol
@@ -0,0 +1,8 @@
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
+// @dev Solmate's ERC20 is used instead of OZ's ERC20 so we can use safeTransferLib for cheaper safeTransfers for
+// ETH and ERC20 tokens
