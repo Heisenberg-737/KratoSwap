424ed05bd5b17cb211d3fa04ebce041250d012fe
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-03-26 11:18:21 +0530

    Initialize in LSSVMPair

--- contracts/LSSVMPair.sol
+++ contracts/LSSVMPair.sol
@@ -3,3 +3,104 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {OwnableWithTransferCallback} from "./lib/OwnableWithTransferCallback.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ICurve} from "./bonding-curves/ICurve.sol";
+import {LSSVMRouter} from "./LSSVMRouter.sol";
+import {ILSSVMPairFactoryLike} from "./ILSSVMPairFactoryLike.sol";
+import {CurveErrorCodes} from "./bonding-curves/CurveErrorCodes.sol";
+import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+
+/// The base contract for an NFT/TOKEN AMM pair
+/// This implements the core swap logic from NFT to TOKEN
+abstract contract LSSVMPair is
+    OwnableWithTransferCallback,
+    ReentrancyGuard,
+    ERC1155Holder
+{
+    enum PoolType {
+        TOKEN,
+        NFT,
+        TRADE
+    }
+
+    // 90%, must <= 1 - MAX_PROTOCOL_FEE (set in LSSVMPairFactory)
+    uint256 internal constant MAX_FEE = 0.90e18;
+
+    // The current price of the NFT
+    // This is generally used to mean the immediate sell price for the next marginal NFT.
+    // However, this should NOT be assumed, as future bonding curves may use spotPrice in different ways.
+    // Use getBuyNFTQuote and getSellNFTQuote for accurate pricing info.
+    uint128 public spotPrice;
+
+    // The parameter for the pair's bonding curve.
+    // Units and meaning are bonding curve dependent.
+    uint128 public delta;
+
+    // The spread between buy and sell prices, set to be a multiplier we apply to the buy price
+    // Fee is only relevant for TRADE pools
+    // Units are in base 1e18
+    uint96 public fee;
+
+    // If set to 0, NFTs/tokens sent by traders during trades will be sent to the pair.
+    // Otherwise, assets will be sent to the set address. Not available for TRADE pools.
+    address payable public assetRecipient;
+
+    // Events
+    event SwapNFTInPair();
+    event SwapNFTOutPair();
+    event SpotPriceUpdate(uint128 newSpotPrice);
+    event TokenDeposit(uint256 amount);
+    event TokenWithdrawal(uint256 amount);
+    event NFTWithdrawal();
+    event DeltaUpdate(uint128 newDelta);
+    event FeeUpdate(uint96 newFee);
+    event AssetRecipientChange(address a);
+
+    // Parameterized Errors
+    error BondingCurveError(CurveErrorCodes.Error error);
+
+    /**
+      Called during pair creation to set initial parameters, only called once by factory to initialize.
+      We verify this by making sure that the current owner is address(0). 
+      The Ownable library we use disallows setting the owner to be address(0), so this condition
+      should only be valid before the first initialize call. 
+     */
+    function initialize(
+        address _owner,
+        address payable _assetRecipient,
+        uint128 _delta,
+        uint96 _fee,
+        uint128 _spotPrice
+    ) external payable {
+        require(owner() == address(0), "Initialized");
+        __Ownable_init(_owner);
+        __ReentrancyGuard_init();
+
+        ICurve _bondingCurve = bondingCurve();
+        PoolType _poolType = poolType();
+
+        if ((_poolType == PoolType.TOKEN) || (_poolType == PoolType.NFT)) {
+            require(_fee == 0, "Only Trade Pools can have nonzero fee");
+            assetRecipient = _assetRecipient;
+        } else if (_poolType == PoolType.TRADE) {
+            require(_fee < MAX_FEE, "Trade fee must be less than 90%");
+            require(
+                _assetRecipient == address(0),
+                "Trade pools can't set asset recipient"
+            );
+            fee = _fee;
+        }
+        require(_bondingCurve.validateDelta(_delta), "Invalid delta for curve");
+        require(
+            _bondingCurve.validateSpotPrice(_spotPrice),
+            "Invalid new spot price for curve"
+        );
+        delta = _delta;
+        spotPrice = _spotPrice;
+    }
+}
