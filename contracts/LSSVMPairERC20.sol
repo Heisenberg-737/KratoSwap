6606be55f7353e1daab102868459fbf7d2f8111a
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-03-27 19:56:50 +0530

    Added LSSVMPairERC20 & ILSSVMPairFactoryLike

---
+++ contracts/LSSVMPairERC20.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.9;
+
+// Referenced from https://github.com/sudoswap/lssvm/blob/main/src/LSSVMPairERC20.sol
+
+import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {LSSVMPair} from "./LSSVMPair.sol";
+import {ILSSVMPairFactoryLike} from "./ILSSVMPairFactoryLike.sol";
+import {LSSVMRouter} from "./LSSVMRouter.sol";
+import {ICurve} from "./bonding-curves/ICurve.sol";
+import {CurveErrorCodes} from "./bonding-curves/CurveErrorCodes.sol";
+
+// An NFT/Token pair where the token is an ERC20
+abstract contract LSSVMPairERC20 is LSSVMPair {
+    using SafeTransferLib for ERC20;
+
+    uint256 internal constant IMMUTABLE_PARAMS_LENGTH = 81;
+
+    // Returns the ERC20 token associated with the pair
+    function token() public pure returns (ERC20 _token) {
+        uint256 paramsLength = _immutableParamsLength();
+        assembly {
+            _token := shr(
+                0x60,
+                calldataload(add(sub(calldatasize(), paramsLength), 61))
+            )
+        }
+    }
+
+    // Used to pull a token input and pay the protocol fee for an ERC20 pair
+    function _pullTokenInputAndPayProtocolFee(
+        uint256 inputAmount,
+        bool isRouter,
+        address routerCaller,
+        ILSSVMPairFactoryLike _factory,
+        uint256 protocolFee
+    ) internal override {
+        require(msg.value == 0, "ERC20 pair");
+
+        ERC20 _token = token();
+        address _assetRecipient = getAssetRecipient();
+
+        if (isRouter) {
+            // Verify if router is allowed
+            LSSVMRouter router = LSSVMRouter(payable(msg.sender));
+
+            // Locally scoped to avoid stack too deep
+            {
+                (bool routerAllowed, ) = _factory.routerStatus(router);
+                require(routerAllowed, "Not router");
+            }
+
+            // Cache state and then call router to transfer tokens from user
+            uint256 beforeBalance = _token.balanceOf(_assetRecipient);
+            router.pairTransferERC20From(
+                _token,
+                routerCaller,
+                _assetRecipient,
+                inputAmount - protocolFee,
+                pairVariant()
+            );
+
+            // Verify token transfer (protect pair against malicious router)
+            require(
+                _token.balanceOf(_assetRecipient) - beforeBalance ==
+                    inputAmount - protocolFee,
+                "ERC20 not transferred in"
+            );
+
+            router.pairTransferERC20From(
+                _token,
+                routerCaller,
+                address(_factory),
+                protocolFee,
+                pairVariant()
+            );
+        } else {
+            // Transfer tokens directly
+            _token.safeTransferFrom(
+                msg.sender,
+                _assetRecipient,
+                inputAmount - protocolFee
+            );
+
+            // Take protocol fee (if it exists)
+            if (protocolFee > 0) {
+                _token.safeTransferFrom(
+                    msg.sender,
+                    address(_factory),
+                    protocolFee
+                );
+            }
+        }
+    }
+
+    // Used to refund a token to the sender
+    function _refundTokenToSender(uint256 inputAmount) internal override {
+        // Do nothing since we transferred the exact input amount
+    }
+
+    // Used to pay the protocol fee from a given ILSSVMPairFactoryLike
+    function _payProtocolFeeFromPair(
+        ILSSVMPairFactoryLike _factory,
+        uint256 protocolFee
+    ) internal override {
+        // Take protocol fee (if it exists)
+        if (protocolFee > 0) {
+            ERC20 _token = token();
+
+            // Round down to the actual token balance if there are numerical stability issues with the bonding curve calculations
+            uint256 pairTokenBalance = _token.balanceOf(address(this));
+            if (protocolFee > pairTokenBalance) {
+                protocolFee = pairTokenBalance;
+            }
+            if (protocolFee > 0) {
+                _token.safeTransfer(address(_factory), protocolFee);
+            }
+        }
+    }
+
+    // Internal override function that is used to send a token output to a specified recipient
+    function _sendTokenOutput(
+        address payable tokenRecipient,
+        uint256 outputAmount
+    ) internal override {
+        // Send tokens to caller
+        if (outputAmount > 0) {
+            token().safeTransfer(tokenRecipient, outputAmount);
+        }
+    }
+
+    // Returns the length of the immutable parameters stored in the contract
+    function _immutableParamsLength() internal pure override returns (uint256) {
+        return IMMUTABLE_PARAMS_LENGTH;
+    }
+
+    // Allows the owner of the contract to withdraw a specified amount of ERC20 tokens from the contract
+    function withdrawERC20(ERC20 a, uint256 amount)
+        external
+        override
+        onlyOwner
+    {
+        a.safeTransfer(msg.sender, amount);
+
+        if (a == token()) {
+            // emit event since it is the pair token
+            emit TokenWithdrawal(amount);
+        }
+    }
+}
